de577bd3da3b2e0a44fc789d3f830ba4d7234e80
Shree Bhagwat <shree@codeaamy.com>
2024-08-29 18:24:15 +0530

    Added git sntax

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,19 @@ class GitTrial extends StatelessWidget {
     );
   }
 }
+// git branch {branchname} -> to create a new branch 
+// git switch {branchname} -> to switch to a branch
+
+
 
 // locally means in my computer. 
 // Flow of Github: Stagging -> Commit -> Push
+
+
+// git add . -> to add all the files to stagging area
+// git commit -m 'message that we need' -> to commit the code 
+// git push -> to push the code
+// git pull -> pull and update the code. 
+
+
+
